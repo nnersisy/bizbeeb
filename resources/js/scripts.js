fa4aa2ae3494f4c6ac8918d3f284c3b26c4827fb
Nona <nnersisy@usc.edu>
2023-02-26 00:43:21 -0800

    final commit

---
+++ resources/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('js--scroll-to-clothing').click(function(){
+        $('html,body').animate({$('js--section-clothing')});
+    });
+    
+    $('.js--scroll-to-clothing').click(function(){
+        var nav = $('.js--section-clothing');
+        var icon = $('.js--section-clothing');
+    });
+    
+});
